8402f0204077c3986a2d177bece2cc38a72f4f14
Atsushi Matsuo <famlog@gmail.com>
2013-05-26 01:21:04 +0900

    Add AllTestsTest.php

---
+++ Test/Case/AllTestsTest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * AllTests file
+ *
+ * Copyright (c) Atsushi Matsuo, Masayuki Nii
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Atsushi Matsuo, Masayuki Nii
+ * @link          http://www.famlog.jp/imcake/
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+/**
+ * AllTests class
+ *
+ * This test group will run all tests.
+ */
+class AllTests extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+    public static function suite() {
+        $suite = new PHPUnit_Framework_TestSuite('All Tests');
+        
+        $basePath = App::pluginPath('IMCake') . 'Test' . DS . 'Case' . DS;
+        
+        $suite->addTestFile($basePath . 'Controller' . DS . 'Component' . DS . 'IMCakeComponentTest.php');
+        $suite->addTestFile($basePath . 'View' . DS . 'Helper' . DS . 'IMCakeHelperTest.php');
+        return $suite;
+    }
+}
